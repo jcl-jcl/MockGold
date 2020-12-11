da445a41e594cab773950d9134fa7c1af211f72b
jcl-jcl <779150813@qq.com>
2020-12-11 09:27:07 +0800

    创建项目

---
+++ src/main/java/com/example/mockgold/MockGoldApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockgold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockGoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockGoldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mockgold/MockGoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockgold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockGoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
